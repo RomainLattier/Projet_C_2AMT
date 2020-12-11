106adf7785b72a1bd39886af56fa04a938991de0
phelma <phelma@localhost.localdomain>
2020-12-11 17:25:46 +0100

    validation ++ du simulateur

--- Projet_C/Simulateur.cpp
+++ Projet_C/Simulateur.cpp
@@ -9,16 +9,16 @@
 #include "/Parser_gate/Parser_gate.h"
 
 //include des gate
-#include "/Porte/Gate.h"
+#include "../Porte/Gate.h"
 
-#include "/Porte/Gate_comb.h"
-#include "/Porte/Combinatoire/And.h"
-#include "/Porte/Combinatoire/Inv.h"
-#include "/Porte/Combinatoire/Or.h"
-#include "/Porte/Combinatoire/Nand.h"
-#include "/Porte/Combinatoire/Nor.h"
-#include "/Porte/Combinatoire/Xor.h"
-#include "/Porte/Combinatoire/Xnor.h"
+#include "../Porte/Gate_comb.h"
+#include "../Porte/Combinatoire/And.h"
+#include "../Porte/Combinatoire/Inv.h"
+#include "../Porte/Combinatoire/Or.h"
+#include "../Porte/Combinatoire/Nand.h"
+#include "../Porte/Combinatoire/Nor.h"
+#include "../Porte/Combinatoire/Xor.h"
+#include "../Porte/Combinatoire/Xnor.h"
 
 #include "../Porte/Gate_out.h"
 
