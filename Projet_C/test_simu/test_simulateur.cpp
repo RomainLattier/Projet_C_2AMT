0a6b340debf3600063a8cb01c28335e380678d39
phelma <phelma@localhost.localdomain>
2020-12-16 12:06:11 +0100

    modif minime

--- Projet_C/test_simu/test_simulateur.cpp
+++ Projet_C/test_simu/test_simulateur.cpp
@@ -38,9 +38,9 @@ int main(){
   map<string,vector<int>* > m_stimulis; //map des différents stimuls clé = entrée vector = valeur
   vector<int> v_duree_delta; //vector de la duréee de chaque stimulis
 
-  string path_stru = "test2_mux.dot";
-  string path_stimuli = "test2_stimulis.json";
-  string path_file_out = "test2.json";
+  string path_stru = "test1.dot";
+  string path_stimuli = "test1_stimulis.json";
+  string path_file_out = "test1.json";
   // cout <<" Donnez le chemin du fichier structure du circuit (fichier .dot)"<<endl;
   // cin >> path_stru;
   // cout <<" Donnez le chemin du fichier stimulis en entrée du circuit (fichier wavedrom avec extansion .json)"<<endl;
