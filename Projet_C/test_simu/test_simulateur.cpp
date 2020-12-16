4f23249868f3a53ad47e5b2f2e8a53af0c05b9d2
phelma <phelma@localhost.localdomain>
2020-12-16 14:21:47 +0100

    un peu de rangement

--- Projet_C/test_simu/test_simulateur.cpp
+++ Projet_C/test_simu/test_simulateur.cpp
@@ -38,9 +38,9 @@ int main(){
   map<string,vector<int>* > m_stimulis; //map des différents stimuls clé = entrée vector = valeur
   vector<int> v_duree_delta; //vector de la duréee de chaque stimulis
 
-  string path_stru = "test1.dot";
-  string path_stimuli = "test1_stimulis.json";
-  string path_file_out = "test1.json";
+  string path_stru = "./fichier_test/test4.dot";
+  string path_stimuli = "./fichier_test/test4_stimulis.json";
+  string path_file_out = "./fichier_test/test4.json";
   // cout <<" Donnez le chemin du fichier structure du circuit (fichier .dot)"<<endl;
   // cin >> path_stru;
   // cout <<" Donnez le chemin du fichier stimulis en entrée du circuit (fichier wavedrom avec extansion .json)"<<endl;
